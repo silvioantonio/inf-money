c9f57acbc6dde3fbed4726813d441d39545fcf94
silvioantonio <oliveira_0000@hotmail.com>
2020-03-20 14:14:03 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/com/silvio/infmoney/InfmoneyApplication.java
@@ -0,0 +1,13 @@
+package com.silvio.infmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfmoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfmoneyApplication.class, args);
+	}
+
+}
