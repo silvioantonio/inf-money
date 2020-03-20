bdacbb7842eb7339dfa3d3ef37b6ca64c72b9b38
silvioantonio <oliveira_0000@hotmail.com>
2020-03-20 15:46:56 -0300

    Adicionando recurso GET

---
+++ src/main/java/com/silvio/infmoney/resource/CategoriaResource.java
@@ -0,0 +1,25 @@
+package com.silvio.infmoney.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.silvio.infmoney.model.Categoria;
+import com.silvio.infmoney.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@GetMapping
+	public List<Categoria> listarTodos () {
+		return categoriaRepository.findAll();
+	}
+	
+}
