e872e22e0d47a5d94dcec8264266a3e2bb0593b1
silvioantonio <oliveira_0000@hotmail.com>
2020-03-20 16:34:32 -0300

    Adicionado recurso para POST e retorno com header location configurado

--- src/main/java/com/silvio/infmoney/resource/CategoriaResource.java
+++ src/main/java/com/silvio/infmoney/resource/CategoriaResource.java
@@ -1,11 +1,20 @@
 package com.silvio.infmoney.resource;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.silvio.infmoney.model.Categoria;
 import com.silvio.infmoney.repository.CategoriaRepository;
@@ -20,6 +29,36 @@ public class CategoriaResource {
 	@GetMapping
 	public List<Categoria> listarTodos () {
 		return categoriaRepository.findAll();
+	}
+	
+	@GetMapping("/{codigo}")
+	public Optional<Categoria> buscarPeloCodigo (@PathVariable Long codigo) {
+		return categoriaRepository.findById(codigo);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> criar (@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		
+		/**
+		 * ServletUriComponentsBuilder == helper do spring
+		 * fromCurrentRequestUri() == pega apartir da requisiçao atual(no caso "/categorias")
+		 * path("/{codigo}") == adicionar a URI o 'codigo'
+		 * buildAndExpand(categoriaSalva.getCodigo()).toUri() == adiciona esse 'codigo' na URI
+		 */
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequestUri()
+				.path("/{codigo}")
+				.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		
+		/**
+		 * Setta o Header 'Location' com essa URI
+		 * O 'Location' é usado para fazer redirect ou quando um novo recurso é criado!
+		 */
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categoriaSalva);
+		
 	}
 	
 }
